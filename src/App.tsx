b7b4884e4fa5fb6c4f5b0e0008071f53664bd2dc
HHector14 <hernandezgarcia.hectorf@gmail.com>
2025-12-29 15:02:50 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <div className="Card">
+
+        <img className='avatar' src="https://ik.imagekit.io/hpapi/harry.jpg" alt="hh" />
+        <img className='frame' src="./card.png" alt="" />
+    </div>
+
+    </>
+  )
+}
+
+export default App
