df8b0bdc1dee704f2c798505347259f36fdf37b8
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-02 09:59:38 -0600

    layout added

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./views/Home"
+import Layout from "./layouts/Layout"
 export default function AppRouter() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
