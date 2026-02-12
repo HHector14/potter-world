f58cb7d0b36bf5691f54904d92740526aa64d609
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-12 14:41:01 -0600

    home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,22 +1,26 @@
-import { Card } from "../components/Card"
-import useFetchAllCharacters from "../hook/useFetchAllCharacters"
+import CardMain from "../components/home/Card-main"
+import CardInfo from "../components/home/Card-info"
+import MiniCardInfo from "../components/home/Mini-card-info"
+import style from '../components/home/Home.module.css'
+
 
 export default function Home() {
-  const { characters, error, loading } = useFetchAllCharacters()
-  if (loading) return <h2>Loading...</h2>
-  if (error) return <h2>Error: {error.message}</h2>
 
   return (
-     <div className="App">
-        {characters.map(c => (
-          <Card
-            key={c.id}
-            id={c.id}
-            name={c.name}
-            image={c.image}
-          />
-        ))}
+    <>
+      <CardMain />
+      <div className={style.containerCardGrid}>
+        <CardInfo icon="fa-people-group" colorbg="purplePink" title="Characters" description="Discover witches and wizards from the magical world" />
+        <CardInfo icon="fa-house" colorbg="redOrange" title="Houses" description="Exploer the four Hogwarts houses and their traits" />
+        <CardInfo icon="fa-magic" colorbg="blueCyan" title="Spells" description="Learn about magical spells and their incantations" />
+        <CardInfo icon="fa-book" colorbg="greenMint" title="Books" description="Browse the complete Harry Potter book series" />
 
       </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+        <MiniCardInfo colorTitle={'#7b3309'} title={'40+'} text={'Character'} />
+        <MiniCardInfo colorTitle={'#59188d'} title={'4'} text={'Houses'} />
+        <MiniCardInfo colorTitle={'#0d542b'} title={'7'} text={'Books'} />
+      </div>
+    </>
   )
 }
