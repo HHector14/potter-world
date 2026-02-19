c817b5933d2ade54251d04d43d6d5368dd1aa1af
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-19 15:20:56 -0600

    Search component deleted from characters

--- src/components/characters/Search.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Characters.module.css'
-export default function Search() {
-  return (
-    <div className={styles.box}>
-      <i className="fa-solid fa-magnifying-glass"></i>
-      <input style={{background: "var(--bg-filter)"}} type="text" placeholder="Search characters..." />
-    </div>
-  )
-}
