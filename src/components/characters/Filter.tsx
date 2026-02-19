9a2fcb4dba33f4e723b00e865fc48b2e92e8852e
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-19 15:26:28 -0600

    Filter with the new filter item

--- src/components/characters/Filter.tsx
+++ src/components/characters/Filter.tsx
@@ -1,12 +1,12 @@
-import style from './Characters.module.css'
+import FilterItem from "../ui/FiterItem"
 export default function Filter() {
   return (
     <>
-      <div style={{backgroundColor: '#bb4d00', color: '#FFF'}} className={`${style.box} ${style.select}`}>All</div>
-      <div className={`${style.box} ${style.select}`}>Gryffindor</div>
-      <div className={`${style.box} ${style.select}`}>Slytherin</div>
-      <div className={`${style.box} ${style.select}`}>Ravenclaw</div>
-      <div className={`${style.box} ${style.select}`}>Hufflepuff</div>
+      <FilterItem label="All" />
+      <FilterItem label="Gryffindor" />
+      <FilterItem label="Slytherin" />
+      <FilterItem label="Ravenclaw" />
+      <FilterItem label="Hufflepuff" />
     </>
   )
 }
