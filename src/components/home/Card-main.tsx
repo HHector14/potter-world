6bf818d7f19b28c1b356703ed629ad78241f10f2
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-19 12:01:51 -0600

    Card main style modified

--- src/components/home/Card-main.tsx
+++ src/components/home/Card-main.tsx
@@ -3,8 +3,8 @@ export default function CardMain() {
   return (
 
     <div className={style.cardMain}>
-      <h2 style={{ fontWeight: '700', fontSize: '60px', margin: '10px auto' }}>Welcome to the Wizarding World</h2>
-      <p style={{ marginTop: 0, color: '#F9D059', fontSize: 'xx-large' }}>Explore the magic of Harry Potter</p>
+      <h2 style={{ fontWeight: '700', fontSize: '3.5rem', margin: '10px auto' }}>Welcome to the Wizarding World</h2>
+      <p style={{ marginTop: 0, color: '#F9D059', fontSize: '1.5rem' }}>Explore the magic of Harry Potter</p>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div style={{ marginRight: '15px' }} className={`${style.button} ${style.orange}`}>Explore Characters</div>
         <div className={`${style.button} ${style.purple}`}>Discover Houses</div>
