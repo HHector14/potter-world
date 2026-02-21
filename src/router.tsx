da0403347ab3087dcfd528ff6929370d4f876bb0
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-20 22:07:04 -0600

    New route to books and houses

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,8 @@ import Home from "./views/Home"
 import Layout from "./layouts/Layout"
 import Characters from "./views/Characters"
 import Spells from "./views/Spells"
+import Houses from "./views/Houses"
+import Books from "./views/Books"
 export default function AppRouter() {
   return(
     <BrowserRouter>
@@ -10,7 +12,9 @@ export default function AppRouter() {
         <Route element={<Layout/>}>
           <Route path="/" element={<Home/>}/>
           <Route path="/characters" element={<Characters/>}/>
+          <Route path="/houses" element={<Houses/>}/>
           <Route path="/spells" element={<Spells/>}/>
+          <Route path="/books" element={<Books/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
