3a1eb9bfdf1f9a7b8f5229f1b71427fc3b34848e
HHector14 <hernandezgarcia.hectorf@gmail.com>
2026-02-21 11:51:41 -0600

    change in the active style for filter item

--- src/components/ui/FiterItem.tsx
+++ src/components/ui/FiterItem.tsx
@@ -1,6 +1,9 @@
+import { NavLink } from 'react-router-dom'
 import style from './Ui.module.css'
-export default function FilterItem({ label } : { label: string }) {
+export default function FilterItem({ url, label }: { url: string, label: string }) {
   return (
-      <div className={`${style.box} ${style.select}`}>{label}</div>
+    <NavLink to={url} className={({ isActive }) => `${style.box} ${style.select} ${isActive ? style.active : ''}`}>
+      {label}
+    </NavLink>
   )
 }
